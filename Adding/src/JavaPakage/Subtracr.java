6e73312e1bab1e055e8960b3df880d3ec9dc9812
Yonas Sh <yonakifle@yahoo.com>
2021-08-26 21:07:46 -0700

    Subtractor

--- Adding/src/JavaPakage/Subtracr.java
+++ Adding/src/JavaPakage/Subtracr.java
@@ -4,7 +4,11 @@ public class Subtracr {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello There")
+		
+		int num1 =5;
+		int num2 = 10;
+		int sub = num2 - num1;
+        System.out.println(sub);
 	}
 
 }
