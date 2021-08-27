387ce4c91c634e411ec2dc3a07ae169f541f3ceb
Yonas Sh <yonakifle@yahoo.com>
2021-08-26 21:05:28 -0700

    subtractor

---
+++ Adding/src/JavaPakage/Subtracr.java
@@ -0,0 +1,10 @@
+package JavaPakage;
+
+public class Subtracr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello There")
+	}
+
+}
